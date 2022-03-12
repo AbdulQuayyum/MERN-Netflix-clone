f73d9d3618a02bf33221dd64c6e2a29f5f9af857
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2022-03-12 21:19:47 +0100

    commit

--- api/index.js
+++ api/index.js
@@ -9,7 +9,11 @@ mongoose
     .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-});
+})
+   .then(() => console.log("DB Connection Successfull"))
+   .catch((err) => {
+   console.error(err);
+  });
 
 app.listen(8800, () => {
     console.log("Backend server is running")
